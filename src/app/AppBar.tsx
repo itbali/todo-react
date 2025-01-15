7d1aadca5b3a694377034ddac7165eecaa1c8077
passrise <ich.nemoroz@gmail.com>
2025-01-15 19:56:18 +0100

    feature/header-with-theme-switcher
    - add fixed position to header toolbar

--- src/app/AppBar.tsx
+++ src/app/AppBar.tsx
@@ -1,15 +1,17 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import {
+	AppBar,
+	Box,
+	Toolbar,
+	Typography,
+	Button,
+	IconButton,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const ButtonAppBar = () => {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
+			<AppBar position="fixed">
 				<Toolbar>
 					<IconButton
 						size="large"
