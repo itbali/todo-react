877b4ad330307313244e84b6ffdb195f86aa5d99
passrise <ich.nemoroz@gmail.com>
2025-01-15 18:43:00 +0100

    feature/header-with-theme-switcher
    - add Material UI, Material Icons, Roboto Font
    - add font imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from '../shared/assets/react.svg';
 import viteLogo from '/vite.svg';
 import githubLogo from '../shared/assets/github.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
 	const [count, setCount] = useState(0);
