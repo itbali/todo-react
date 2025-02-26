d42323159b4f0d54a05a7f05174fa3fbfd5cb308
passrise <ich.nemoroz@gmail.com>
2025-02-26 06:52:46 +0100

    feature/rtk-integration
    - refactor formatDistanceToNow on todos page

--- src/shared/utils/DateConverter.ts
+++ src/shared/utils/DateConverter.ts
@@ -2,7 +2,7 @@ export const dateConverter = (date: string) => {
 	return new Date(date).toDateString();
 };
 
-export const formatDistanceToNow = (date: string) => {
+export const formatDistanceToNow = (date: string, includeSeconds?: boolean) => {
 	const currentDate = Date.now();
 	const dateFromLastUpdate = new Date(date).getTime();
 
@@ -15,31 +15,51 @@ export const formatDistanceToNow = (date: string) => {
 	const months = Math.floor(days / 30);
 	const years = Math.floor(days / 365);
 
-	if (seconds <= 30) {
-		return `less than a minute`;
-	} else if (minutes < 2) {
-		return `1 minute`;
-	} else if (minutes <= 45) {
-		return `${minutes} minutes ago`;
-	} else if (hours < 2) {
-		return 'about 1 hour';
-	} else if (hours <= 24) {
-		return `about ${hours} hours`;
-	} else if (hours < 42) {
-		return '1 day';
-	} else if (days <= 30) {
-		return `${days} days`;
-	} else if (days < 45) {
-		return 'about 1 month';
-	} else if (days < 60) {
-		return 'about 2 months';
-	} else if (days < 365) {
-		return `${months} months`;
-	} else if (months <= 15) {
-		return 'about 1 year';
-	} else if (months <= 21) {
-		return 'over 1 year';
-	} else if (years < 2) {
-		return 'almost 2 year';
+	if (includeSeconds === true) {
+		switch (true) {
+			case seconds < 5:
+				return 'less than 5 seconds';
+			case seconds < 10:
+				return 'less than 10 seconds';
+			case seconds < 20:
+				return 'less than 20 seconds';
+			case seconds < 40:
+				return 'half a minute';
+			case seconds < 60:
+				return 'less than a minute';
+			case seconds < 90:
+				return '1 minute';
+		}
+	}
+
+	switch (true) {
+		case seconds <= 30:
+			return 'less than a minute';
+		case minutes < 2:
+			return '1 minute';
+		case minutes <= 45:
+			return `${minutes} minutes ago`;
+		case hours < 2:
+			return 'about 1 hour';
+		case hours < 24:
+			return `about ${hours} hours`;
+		case hours < 42:
+			return '1 day';
+		case days <= 30:
+			return `${days} days`;
+		case days < 45:
+			return 'about 1 month';
+		case days < 60:
+			return 'about 2 months';
+		case days < 365:
+			return `${months} months`;
+		case months <= 15:
+			return 'about 1 year';
+		case months <= 21:
+			return 'over 1 year';
+		case years < 2:
+			return 'almost 2 year';
+		default:
+			return `about ${years} years`;
 	}
 };
